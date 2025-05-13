7c40727c91edcb82232751d5c284f92df0296b74
taligrayzel <102834570+taligrayzel@users.noreply.github.com>
2025-05-13 13:21:46 +0300

    Delete Unit Tests/DBConfig.h

--- Unit Tests/DBConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef DBCONFIG_H
-#define DBCONFIG_H
-
-#define WIFI_SSID "HOTBOX 4-8B80"
-#define WIFI_PASS "QZT7LW79HPRS"
-#define DB_URL    "https://r2d2-maze-default-rtdb.firebaseio.com/"
-#define DB_SECRET "EgzKV3U8EXEddAaHc615RsxB70zi7Ywx1NCfSHvr"
-
-#endif
